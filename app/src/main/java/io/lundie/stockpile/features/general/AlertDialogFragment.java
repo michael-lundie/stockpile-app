452366018b848d30059859fc1311ddf83a3647c3
Michael Lundie <michael@tal.tokyo>
2020-04-16 10:57:19 +0900

    Edit to AlertDialogFragment.

--- app/src/main/java/io/lundie/stockpile/features/general/AlertDialogFragment.java
+++ app/src/main/java/io/lundie/stockpile/features/general/AlertDialogFragment.java
@@ -2,10 +2,17 @@ package io.lundie.stockpile.features.general;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.DialogFragment;
+
+import io.lundie.stockpile.R;
 
 public class AlertDialogFragment extends DialogFragment {
 
@@ -40,21 +47,32 @@ public class AlertDialogFragment extends DialogFragment {
             String negBtnLabel = getArguments().getString(NEG_BTN_KEY);
             String message = getArguments().getString(MSG_KEY);
 
-            return new AlertDialog.Builder(getActivity())
+            AlertDialog alertDialog =  new AlertDialog.Builder(getActivity())
                     .setTitle(title)
                     .setMessage(message)
                     .setPositiveButton(posBtnLabel,
                             (dialog, whichButton) -> {
-                        dialogListener.onPositiveClicked();
-                        dismiss();
+                                dialogListener.onPositiveClicked();
+                                AlertDialogFragment.this.dismiss();
                             }
                     )
                     .setNegativeButton(negBtnLabel,
                             (dialog, whichButton) -> dismiss()
                     )
                     .create();
-    }
 
+            alertDialog.setOnShowListener((DialogInterface dialogInterface) -> {
+                Button negButton = alertDialog.getButton(DialogInterface.BUTTON_NEGATIVE);
+
+                LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.WRAP_CONTENT,
+                        LinearLayout.LayoutParams.WRAP_CONTENT
+                );
+                params.setMargins((int)getResources().getDimension(R.dimen.keyline_1),0,(int)getResources().getDimension(R.dimen.keyline_1),0);
+                negButton.setLayoutParams(params);
+            });
+            return alertDialog;
+    }
 
     private void setDialogListener(DialogListener dialogListener) {
         this.dialogListener = dialogListener;
